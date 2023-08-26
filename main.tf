b55f352f48a577b0bc16a99625785532ce782276
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2023-08-25 23:46:40 -0500

    change "block" to "blog" - syntax error

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.block.id] 
+  vpc_security_group_ids = [aws_security_group.blog.id] 
   tags = {
     Name = "HelloWorld"
   }
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol     = "tcp"
   cidr_block   = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.block.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   protocol     = "tcp"
   cidr_block   = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.block.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
@@ -60,6 +60,6 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol     = "-1"
   cidr_block   = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.block.id
+  security_group_id = aws_security_group.blog.id
 }
 
