24593e342acb7ad2f31883bc1ff6e61d376b8bce
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2023-08-28 21:21:56 -0500

    Update main.tf
    create dev vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,18 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -32,7 +42,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog"
 
-  vpc_id                = data.aws_vpc.default.id
+  vpc_id                = module.vpc.public_subnets[0]
   ingress_rules         = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
   egress_rules         = ["all-all"]
