ace70aa009056950f0751860572a39d49ad75c8d
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2023-08-28 21:25:02 -0500

    Update main.tf
    add public_subnets

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ module "vpc" {
 
   name = "dev"
   cidr = "10.0.0.0/16"
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
